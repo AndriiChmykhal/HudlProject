8124ec928049a4ce9352ad879c41b0c977224907
Andrii Chmykhal <andrii.chmykhal@gmail.com>
2025-02-16 14:29:36 +0000

    extracted valid credentials to .env

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,11 +1,17 @@
-from wsgiref.validate import validator
+import os
+from dotenv import load_dotenv
 
-base_url = "https://www.hudl.com"
-valid_email = "andrii.chmykhal@gmail.com"
-privacy_policy_url = "https://www.hudl.com/privacy"
-terms_of_service_url = "https://www.hudl.com/terms"
-valid_password = "g%PY2%c*g#ZF_GY"
+load_dotenv()
+
+valid_email = os.getenv("VALID_EMAIL")
+valid_password = os.getenv("VALID_PASSWORD")
+invalid_email = "@com"
+invalid_password = "1"
 wrong_email = "test+123@test.com"
 wrong_password = "Qwert123!"
+
+base_url = "https://www.hudl.com"
+privacy_policy_url = "https://www.hudl.com/privacy"
+terms_of_service_url = "https://www.hudl.com/terms"
 
 screen_resolution = {"width": 1920, "height": 1080}
